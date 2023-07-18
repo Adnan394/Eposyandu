76dfdcf7b539ac9fb0e2a241a54ea97f2f6917b1
adnan <adnanega82@gmail.com>
2023-07-18 14:44:20 +0700

    settings menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,66 +15,100 @@
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css">
+
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  
+  <style>
+    /* Sidebar */
+    .sidebar {
+      position: fixed;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      z-index: 100;
+      
+      overflow-x: hidden;
+      /* overflow-y: auto; */
+      background-color: #f8f9fa;
+      border-right: 1px solid #dee2e6;
+    }
+
+    /* Sidebar Menu */
+    .nav-link {
+      color: #fff;
+      font-weight: 500;
+      border-radius: 5px;
+    }
+
+    .nav-link:hover {
+        background: rgb(25, 180, 84);
+        color: #fff;
+    }
+
+    /* Active Menu Item */
+    .active {
+        background: rgb(25, 180, 84);
+        color: #fff;
+    }
+
+    /* Content */
+    main {
+      margin-left: 140px;
+      padding: 20px;
+    }
+  </style>
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+    <div class="container-fluid">
+        <div class="row">
+          <!-- Sidebar -->
+          <aside class="col-md-3 col-lg-2 sidebar bg-success">
+            <ul class="nav flex-column justify-content-start">
+              <li class="nav-item mb-3 border-bottom border-sm pb-3 mt-3">
+                <a class="nav-link " href="" >
+                  Menu 1
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
+              </li>
+              <!-- Menu Item 1 -->
+              <li class="nav-item">
+                <a class="nav-link" href="">
+                  <i class="fas fa-home me-2"></i>
+                  Menu 1
+                </a>
+              </li>
+    
+              <!-- Menu Item 2 -->
+              <li class="nav-item">
+                <a class="nav-link" href="">
+                  <i class="fas fa-cogs me-2"></i>
+                  Menu 2
+                </a>
+              </li>
+              <!-- Menu Item 2 -->
+              <li class="nav-item">
+                <a class="nav-link" href="">
+                  <i class="fas fa-cogs me-2"></i>
+                  Menu 2
+                </a>
+              </li>
+            </ul>
+          </aside>
+    
+          <!-- Content -->
+          
+        </div>
+    </div>
+    <div id="app">
         <main class="py-4">
             @yield('content')
         </main>
     </div>
+        <!-- Bootstrap Bundle with Popper.js -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Font Awesome Icons -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js"></script> 
 </body>
 </html>
